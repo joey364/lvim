a0604f69ffaee56bb3a062111d6a3bc96f1dd709
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-30 20:02:36 +0000

    install git-blame.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,6 +71,16 @@ lvim.plugins = {
 		"p00f/nvim-ts-rainbow",
 	},
 
+	-- git
+	{
+		"f-person/git-blame.nvim",
+		event = "BufRead",
+		config = function()
+			vim.cmd("highlight default link gitblame SpecialComment")
+			vim.g.gitblame_enabled = 0
+		end,
+	},
+
 	-- general
 	{
 		"ray-x/lsp_signature.nvim",
