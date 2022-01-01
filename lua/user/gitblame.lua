44547944a302e16fe4373abf36a9bd7b3eea9be8
joey364 <71462893+joey364@users.noreply.github.com>
2022-01-01 17:01:45 +0000

    add config for git-blame.nvim

---
+++ lua/user/gitblame.lua
@@ -0,0 +1,5 @@
+-- vim.cmd("highlight default link gitblame SpecialComment")
+vim.g.gitblame_enabled = 1
+vim.g.gitblame_date_format = "%r"
+vim.g.gitblame_highlight_group = "LineNr"
+vim.g.gitblame_message_template = "<author> • <date> • <summary>"
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -74,10 +74,8 @@ lvim.plugins = {
 	-- git
 	{
 		"f-person/git-blame.nvim",
-		event = "BufRead",
 		config = function()
-			vim.cmd("highlight default link gitblame SpecialComment")
-			vim.g.gitblame_enabled = 0
+			require("user.gitblame")
 		end,
 	},
 
