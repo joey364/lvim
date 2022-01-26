68fc8cb6af5f93fde4db7e23c4d7a72cf3d39c5d
joey364 <71462893+joey364@users.noreply.github.com>
2022-01-26 13:29:14 +0000

    add markdown preview plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,131 +1,137 @@
 -- plugins go here
 lvim.plugins = {
-	--themes
-	-- { "folke/tokyonight.nvim" },
-	-- { "glepnir/zephyr-nvim" },
-	{ "lunarvim/colorschemes" },
+  --themes
+  -- { "folke/tokyonight.nvim" },
+  -- { "glepnir/zephyr-nvim" },
+  { "lunarvim/colorschemes" },
 
-	--lsp enhancement
-	{
-		"tzachar/cmp-tabnine",
-		config = function()
-			local tabnine = require("cmp_tabnine.config")
-			tabnine:setup({
-				max_lines = 1000,
-				max_num_results = 20,
-				sort = true,
-			})
-		end,
-		run = "./install.sh",
-		requires = "hrsh7th/nvim-cmp",
-	},
-	{
-		"aca/emmet-ls",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local configs = require("lspconfig/configs")
+  --lsp enhancement
+  {
+    "tzachar/cmp-tabnine",
+    config = function()
+      local tabnine = require "cmp_tabnine.config"
+      tabnine:setup {
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+      }
+    end,
+    run = "./install.sh",
+    requires = "hrsh7th/nvim-cmp",
+  },
+  {
+    "aca/emmet-ls",
+    config = function()
+      local lspconfig = require "lspconfig"
+      local configs = require "lspconfig/configs"
 
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
-			capabilities.textDocument.completion.completionItem.resolveSupport = {
-				properties = {
-					"documentation",
-					"detail",
-					"additionalTextEdits",
-				},
-			}
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+          "documentation",
+          "detail",
+          "additionalTextEdits",
+        },
+      }
 
-			if not lspconfig.emmet_ls then
-				configs.emmet_ls = {
-					default_config = {
-						cmd = { "emmet-ls", "--stdio" },
-						filetypes = {
-							"html",
-							"css",
-							"javascript",
-							"typescript",
-							"typescriptreact",
-							"javascriptreact",
-						},
-						root_dir = function(fname)
-							return vim.loop.cwd()
-						end,
-						settings = {},
-					},
-				}
-			end
-			lspconfig.emmet_ls.setup({ capabilities = capabilities })
-		end,
-	},
+      if not lspconfig.emmet_ls then
+        configs.emmet_ls = {
+          default_config = {
+            cmd = { "emmet-ls", "--stdio" },
+            filetypes = {
+              "html",
+              "css",
+              "javascript",
+              "typescript",
+              "typescriptreact",
+              "javascriptreact",
+            },
+            root_dir = function(fname)
+              return vim.loop.cwd()
+            end,
+            settings = {},
+          },
+        }
+      end
+      lspconfig.emmet_ls.setup { capabilities = capabilities }
+    end,
+  },
 
-	--treesitter
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		"p00f/nvim-ts-rainbow",
-	},
+  --treesitter
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
 
-	-- git
-	{
-		"f-person/git-blame.nvim",
-		config = function()
-			require("user.gitblame")
-		end,
-	},
+  -- git
+  {
+    "f-person/git-blame.nvim",
+    config = function()
+      require "user.gitblame"
+    end,
+  },
 
-	-- general
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-	},
-	{
-		"metakirby5/codi.vim",
-		cmd = "Codi",
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({ "*" }, {
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = true, -- CSS rgb() and rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-			})
-		end,
-	},
-	-- {
-	-- 	"Pocco81/AutoSave.nvim",
-	-- 	config = function()
-	-- 		require("autosave").setup()
-	-- 	end,
-	-- },
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufRead",
-		setup = function()
-			vim.g.indentLine_enabled = 1
-			vim.g.indent_blankline_char = "▏"
-			vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
-			vim.g.indent_blankline_buftype_exclude = { "terminal" }
-			vim.g.indent_blankline_show_trailing_blankline_indent = false
-			vim.g.indent_blankline_show_first_indent_level = false
-		end,
-	},
-	{
-		"folke/todo-comments.nvim",
-		config = function()
-			require("user.todo_comments").config()
-		end,
-	},
+  -- general
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "metakirby5/codi.vim",
+    cmd = "Codi",
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "*" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    ft = "markdown",
+  },
+
+  -- {
+  -- 	"Pocco81/AutoSave.nvim",
+  -- 	config = function()
+  -- 		require("autosave").setup()
+  -- 	end,
+  -- },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufRead",
+    setup = function()
+      vim.g.indentLine_enabled = 1
+      vim.g.indent_blankline_char = "▏"
+      vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+      vim.g.indent_blankline_buftype_exclude = { "terminal" }
+      vim.g.indent_blankline_show_trailing_blankline_indent = false
+      vim.g.indent_blankline_show_first_indent_level = false
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    config = function()
+      require("user.todo_comments").config()
+    end,
+  },
 }
