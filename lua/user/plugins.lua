203a3fdcbab6018528b1c45c7755fe37448b8624
joey364 <71462893+joey364@users.noreply.github.com>
2022-07-20 11:51:12 +0000

    add code runner plugin to plugin list

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -146,4 +146,21 @@ lvim.plugins = {
       require("user.todo_comments").config()
     end,
   },
+  {
+    "CRAG666/code_runner.nvim",
+    config = function()
+      require("code_runner").setup {
+        -- put here the commands by filetype
+        filetype = {
+          c = "gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          cpp = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+          python = "python3 -u",
+          typescript = "deno run",
+          rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+        },
+      }
+    end,
+  },
+  requires = "nvim-lua/plenary.nvim",
 }
