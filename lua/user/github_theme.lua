75f7c850ffbdf48689574e1fd6086873e3837461
joey364 <71462893+joey364@users.noreply.github.com>
2022-08-15 15:23:34 +0000

    update github theme config

--- lua/user/github_theme.lua
+++ lua/user/github_theme.lua
@@ -1,8 +1,17 @@
--- Example config in Lua
-require("github-theme").setup {
-  transparent = false,
+local status_ok, github_theme = pcall(require, "github-theme")
+if not status_ok then
+  return
+end
+
+-- config goes here
+github_theme.setup {
+  transparent = true,
   theme_style = "dark_default",
+  comment_style = "italic",
+  keyword_style = "italic",
   function_style = "italic",
+  -- variable_style = "italic",
+  dark_float = true,
   sidebars = { "qf", "vista_kind", "terminal", "packer" },
 
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
