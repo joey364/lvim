d9434be96f0a79c49dc0e91179d0bfe7d24ae838
joey364 <71462893+joey364@users.noreply.github.com>
2022-11-26 15:20:22 +0000

    filter out lsp warnings for c/c++ files

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,1 +1,8 @@
-cpp.lua
+local notify_filter = vim.notify
+vim.notify = function(msg, ...)
+  if msg:match "warning: multiple different client offset_encodings detected for buffer, this is not supported yet" then
+    return
+  end
+
+  notify_filter(msg, ...)
+end
