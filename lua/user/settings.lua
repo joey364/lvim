222b17a2958ef172e55e84705e59c95a82b8be48
joey364 <71462893+joey364@users.noreply.github.com>
2022-11-26 15:05:45 +0000

    add illuminate options

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,1 +1,6 @@
 vim.opt.relativenumber = true
+
+-- vim-illuminate options
+vim.cmd [[hi def illuminatedWordText gui=bold]]
+vim.cmd [[hi def IlluminatedWordRead gui=bold]]
+vim.cmd [[hi def IlluminatedWordWrite gui=bold]]
